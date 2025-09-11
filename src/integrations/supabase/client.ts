fbfba7ba73b3ae2312f6998c0e370dddf66c33cc
rezdenSSPS <rezdencsgo@gmail.com>
2025-09-11 08:31:04 +0200

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://uwjkbitqnbgovhxxkpgn.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3amtiaXRxbmJnb3ZoeHhrcGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0ODg3NTgsImV4cCI6MjA3MzA2NDc1OH0.fydzlq7R6k03j0aLikYv1OMZiFo0l9hrImgW2HoAXuc";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL!;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
